663683f718dfda57e4b45aa04b598b638b51f883
Siradji Awoual <surajauwal89@gmail.com>
2020-02-20 23:52:31 +0100

    Added css Loaders

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <div className='section'>
+          <h1>React App Skelaton</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HTMLwebplugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HTMLwebplugin({
+      template: './src/index.html'
+    })
+  ]
+};
