225179054ab7fa1794224386fab1605ef27661f6
Siradji Awoual <surajauwal89@gmail.com>
2020-02-23 20:21:14 +0100

    Added Babel Polyfill and Dotenv

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import '@babel/polyfill';
 import App from './App';
 
 ReactDom.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLwebplugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
@@ -26,6 +27,7 @@ module.exports = {
   plugins: [
     new HTMLwebplugin({
       template: './public/index.html'
-    })
+    }),
+    new Dotenv()
   ]
 };
